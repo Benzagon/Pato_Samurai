869bae2de188853812b4e522a9a367fd7a56578c
Benzagon <gonbenzaquen@gmail.com>
2021-10-14 20:48:40 -0300

    MainMenu

---
+++ Test_Proyecto2D_NUEVO/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+
+}
