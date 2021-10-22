e49175eda9c001ce99b1bc5cbd84fc968bed7e3a
DTRecords <91754154+DTRecords@users.noreply.github.com>
2021-10-22 09:01:53 -0300

    MusicSlider

---
+++ Test_Proyecto2D_NUEVO/Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+        var destroy = FindObjectsOfType<test>();
+        for (int i = 1; i < destroy.Length; i++)
+        {
+            Destroy(destroy[i]);
+        }
+	}
+}
