c424871fd0c2ac9ead403454da7f07d48b2bccf2
Gonzalo <46580761@est.ort.edu.ar>
2021-10-22 15:50:37 -0300

    MusicBugFixed

---
+++ Test_Proyecto2D_NUEVO/Assets/Scripts/MusicPlayerScrpit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScrpit : MonoBehaviour {
+
+    AudioSource audSrce;
+    private bool isPlaying = false;
+
+	void Start ()
+    {
+
+        audSrce = GetComponent<AudioSource>();      
+
+        DontDestroyOnLoad(gameObject);
+        var destroy = FindObjectsOfType<MusicPlayerScrpit>();
+        for (int i = 1; i < destroy.Length; i++)
+        {
+            Destroy(destroy[i]);
+        }
+	}
+
+    void Update()
+    {
+        if (isPlaying == false)
+        {
+            audSrce.Play();
+            isPlaying = true;
+        }
+    }
+}
